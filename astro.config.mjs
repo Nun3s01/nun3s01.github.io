d99e3dd934faf2ca3eb68e93a717b88167f284df
Cauã <cauanunes77@yahoo.com>
2026-08-28 12:34:42 -0300

    fix: Removed 'base: /' config in project document.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,4 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://nun3s01.github.io',
-    base: '/nun3s01.github.io',
 });
