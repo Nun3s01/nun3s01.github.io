f6bce19167486977f9808a2417096efd4a41e8cf
Cauã <cauanunes77@yahoo.com>
2026-08-28 23:29:59 -0300

    feat(seo): implements sitemap, robots.txt, and optimized meta tags.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
-    site: 'https://nun3s01.github.io',
+  site: 'https://nun3s01.github.io',
+  integrations: [sitemap()],
 });
